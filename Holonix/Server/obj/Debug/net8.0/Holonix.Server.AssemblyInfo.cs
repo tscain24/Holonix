f1a5131cd003646bee704e71b08c0c30faba2ade
Tyler Cain <tscain11@gmail.com>
2026-02-15 14:22:38 -0500

    migrate dbs and drop sports tables

--- Holonix/Server/obj/Debug/net8.0/Holonix.Server.AssemblyInfo.cs
+++ Holonix/Server/obj/Debug/net8.0/Holonix.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Holonix.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d336e5b91940a397813930ba49d10126bcddeda4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ff3824fce88019e3119e7cab5f317c8967f19b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Holonix.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Holonix.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
